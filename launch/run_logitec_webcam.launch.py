ecc4448ce3e5f8a1ba9035a182a574977d696c21
FabPrez <giuseppefabio.preziosa@polimi.it>
2026-02-03 11:49:28 +0100

    added a py node to run logitec c270

---
+++ launch/run_logitec_webcam.launch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # Absolute path to your venv Python interpreter
+    # python_bin = "/home/fabioprez/projects/template_ws/try_venv/bin/python3"
+
+    return LaunchDescription([
+        Node(
+            package='sensing_utils',
+            executable='logitec_c270_publisher.py',
+            name='logitec_publisher',
+            output='screen',
+            # prefix=python_bin
+        )
+    ])
+
